0dc9fd301444433055fb6cb4c0f5d78013023d42
DevTruce <DevTruce@Gmail.com>
2024-04-20 18:59:04 -0400

    multiple building types

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour {
 
-    [SerializeField] private Transform pfWoodHarvester;
-
     private Camera mainCamera;
+    private BuildingTypeListSO buildingTypeList;
+    private BuildingTypeSO buildingType;
 
     private void Start() {
         mainCamera = Camera.main;
+
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        buildingType = buildingTypeList.list[0];
     }
+
     private void Update() {
+
+        // place wood harvester at current mouse position
         if (Input.GetMouseButtonDown(0))  {
-            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
         }  
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            buildingType = buildingTypeList.list[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y)) {
+            buildingType = buildingTypeList.list[1];
+        }
     }
 
     private Vector3 GetMouseWorldPosition() {
-        // Convert Screen Position Units to World Position Units
+        // convert screen position units to world position units
         Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition.z = 0f;
 
