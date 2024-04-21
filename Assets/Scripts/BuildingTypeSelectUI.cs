293bb024a62c1ca2960417e32c3af1d97f4d1546
DevTruce <DevTruce@Gmail.com>
2024-04-21 00:34:11 -0400

    ui to select harvest buildings and logic to place selected building

---
+++ Assets/Scripts/BuildingTypeSelectUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BuildingTypeSelectUI : MonoBehaviour {
+
+
+
+    private void Awake() {
+        Transform btnTemplate = transform.Find("btnTemplate");
+        btnTemplate.gameObject.SetActive(false);
+
+        BuildingTypeListSO buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+
+        int index = 0;
+        foreach (BuildingTypeSO buildingType in buildingTypeList.list) {
+            Transform btnTransform = Instantiate(btnTemplate, transform);
+            btnTransform.gameObject.SetActive(true);
+
+            float offsetAmount = +160f;
+            btnTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(offsetAmount * index, 0);
+
+            btnTransform.Find("image").GetComponent<Image>().sprite = buildingType.sprite;
+
+            btnTransform.GetComponent<Button>().onClick.AddListener(() => {
+                BuildingManager.Instance.SetActiveBuildingType(buildingType);
+            });
+            index++;
+        }
+    }
+   
+}
